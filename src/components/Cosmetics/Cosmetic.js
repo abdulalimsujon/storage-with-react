cb4a61d90490110a3c82b8ade61c9224c709c56b
abdulalimsujon <alimsujon12@gmail.com>
2022-07-02 17:50:47 +0600

    handler with parameter

--- src/components/Cosmetics/Cosmetic.js
+++ src/components/Cosmetics/Cosmetic.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import './cosmetic.css';
 
 const Cosmetic = (props) => {
 
     const { name, price, id } = props.cosmetic;
+    const AddToCart = (id) => {
+        console.log('item added', id);
+    }
     return (
-        <div>
+        <div class='product'>
             <h1>buy this :{name} </h1>
             <h2>Only for :{price}</h2>
             <h3>id : {id}</h3>
+            <button onClick={() => AddToCart(id)}>Add to Cart</button>
         </div>
     );
 };

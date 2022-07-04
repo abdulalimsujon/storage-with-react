9d892854e6f68e5d924b428297fb13db7176ad9f
abdulalimsujon <alimsujon12@gmail.com>
2022-07-05 02:14:31 +0600

    local storage with valuable name

--- src/components/utilities/fakedb.js
+++ src/components/utilities/fakedb.js
@@ -1,14 +1,37 @@
 const addToDb = id => {
 
-    const quantity = localStorage.getItem(id);
+    let shoppingCart = {};
+
+    //get shopping cart from local storage
+    const storedCard = localStorage.getItem('shopping-cart');
+
+    if (storedCard) {
+        shoppingCart = JSON.parse(storedCard);
+
+    } else {
+        shoppingCart = {};
+
+    }
+
+
+
+    // add quantity
+
+    const quantity = shoppingCart[id];
     if (quantity) {
         // console.log('already exist');
         const newQuantity = parseInt(quantity) + 1;
-        localStorage.setItem(id, newQuantity);
+
+        shoppingCart[id] = newQuantity;
+        // localStorage.setItem(id, newQuantity);
     } else {
-        localStorage.setItem(id, 1);
+
+        shoppingCart[id] = 1;
+        //localStorage.setItem(id, 1);
         //console.log('new item');
     }
+
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
 
 }
 
